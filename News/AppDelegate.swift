294a2b5ef0f9ca4c53678e78807b37844d962a4b
Ahmed Hesham <alimido94@hotmail.com>
2019-07-27 13:20:59 +0200

    -- fix some code alignment

--- News/AppDelegate.swift
+++ News/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return Reachability()
     }()
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
